68d8f5669f331c0b14806b2757a8e481d142af7c
Your Name <you@example.com>
2019-04-20 18:34:29 +0300

    last

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ class BinaryTest(unittest.TestCase):
             goal = random.randrange(1, 1000)
             bool_ = goal in self.value
             _, bool_test = c.binary_search(self.value, goal)
-            if bool_ is bool_test:
+            if not bool_ is bool_test:
                 raise RuntimeError(f'level{i} - {goal} not in {self.value}')
 
 
